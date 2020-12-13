8bdc04d5b0a24a5ea41f2ed9e10b404514be8872
yuusuke16 <murakai1016@gmail.com>
2020-12-13 21:38:26 +0900

    Calendar2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 'use strict'
+
+
+{
+  const today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+
+  function getCalendarHead() {
+    const dates =[];
+    const d = new Date(year, month,0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    
+  }
+  function getCalendarBody() {
+    const dates = []; // date: 日付, day: 曜日
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: false,
+      });
+    }
+
+    console.log(dates);
+  }
+
+  getCalendarBody();
+}
